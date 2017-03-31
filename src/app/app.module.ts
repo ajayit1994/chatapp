a83f27d7269e5ec5d568fc8b008245c339a124be
ajayit1994 <ajay.mudaliyar1994@gmail.com>
2017-03-31 18:40:59 +0530

    'first'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppHomeComponent } from './pages/app-home/app-home.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import {UserService} from './shared/services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHomeComponent,
+    UserListComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
